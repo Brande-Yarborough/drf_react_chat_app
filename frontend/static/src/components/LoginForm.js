d668639089e7b534c7c182f13af77198a9de3b4c
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-18 14:14:06 -0500

    adds login form

--- frontend/static/src/components/LoginForm.js
+++ frontend/static/src/components/LoginForm.js
@@ -1,1 +1,50 @@
- 
+import { useState } from "react";
+import Cookies from "js-cookie";
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  password: "",
+};
+
+function LoginForm(props) {
+  const [state, setState] = useState(INITIAL_STATE);
+
+  const handleInput = (e) => {
+    const { name, value } = e.target; //value of this inside event listener is event.target, value of this in fat arrow is LoginForm
+    setState((prevState) => ({
+      //previous changes to state will be executed in correct order, that is why we use function here
+      //go get previous object, and now I'm going to update it
+      //you would lose other properties if didn't spread out prevState
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleError = (err) => {
+    console.warn(err);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+      body: JSON.stringify(state), //state is object that has all properties to send up on post request: name, email, pass
+    };
+    const response = await fetch("/dj-rest-auth/login/", options).catch(
+      handleError
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    const data = await response.json(); //when we login and are registered we get key
+    Cookies.set("Authorization", `Token ${data.key}`); //set auth cookie and value is token with key value when logged in and registered
+    //when logout, need to remove cookie
+    props.setAuth(true);
+  };
+}
+
+export default LoginForm;
